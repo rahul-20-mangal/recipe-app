dd3cda52c7a83eb1c456c0f3ebf62d677db18ae0
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-27 11:35:04 +0530

    Create models and register on admin page

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from recipe.models import Recipe, Ingredient
 
-# Register your models here.
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 5   
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [IngredientInline]
+
+
+admin.site.register(Recipe, RecipeAdmin)
--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Recipe(TimeStampedModel):
+    title = models.CharField(max_length=512)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    Veg_CHOICES = (
+        ('Veg', 'Veg'),
+        ('Vegan', 'Vegan'),
+        ('Non-Veg', 'Non-Veg'),
+    )
+    veg_or_other = models.CharField(max_length=10, choices=Veg_CHOICES)
+
+    image = models.ImageField(upload_to='image')
+    cuisine = models.CharField(max_length=2000, null=True, blank=True)
+
+    difficulty_choices = (
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('5','5'),
+    )
+    difficulty_level = models.CharField(max_length=1, choices=difficulty_choices)
+
+    instructions = models.TextField()
+    servings = models.IntegerField(default=0, blank=True)
+    preparation_time = models.TimeField(null=True, blank=True)
+    total_time = models.TimeField(null=True, blank=True)
+    calories = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return '{} by {}'.format(self.title, self.author)
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200)
+    quantity = models.DecimalField(decimal_places=2, max_digits=5, default=0, blank=True)
+    measurement = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
